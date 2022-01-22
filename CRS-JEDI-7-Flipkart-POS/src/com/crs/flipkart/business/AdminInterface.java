c9a71fc9a18c107556de3bc8d4fd4fb94ce823fb
Ayush <aayushjn.0248@gmail.com>
2022-01-22 16:08:17 +0530

    Defined AdminInterface

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/AdminInterface.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/AdminInterface.java
@@ -3,10 +3,32 @@
  */
 package com.crs.flipkart.business;
 
+import java.util.ArrayList;
+
+import com.crs.flipkart.bean.Challan;
+import com.crs.flipkart.bean.Course;
+import com.crs.flipkart.bean.Professor;
+import com.crs.flipkart.bean.SemesterRegistration;
+import com.crs.flipkart.bean.Student;
+import com.crs.flipkart.bean.StudentRegisteredCourses;
+import com.crs.flipkart.validator.StandardResponse;
+
 /**
  * @author HP
  *
  */
 public interface AdminInterface {
-
+	public StandardResponse addCourse(Course course);
+	public StandardResponse removeCourse(int courseId);
+	public StandardResponse updateCourse(Course course);
+	public StandardResponse addProfessor(Professor professor);
+	public StandardResponse removeProfessor(int professorId);
+	public StandardResponse updateProfessor(Professor professor);
+	public StandardResponse approveStudentRegistration(Student student);
+	public ArrayList<Course> viewAllCourses();
+	public void activateGradeCard();
+	public ArrayList<Student> viewAllStudents();
+	public ArrayList<Professor> viewAllProfessors();
+	public ArrayList<StudentRegisteredCourses> allocatePendingCourses();
+	public Challan generateChallan(SemesterRegistration semesterRegistration);
 }
